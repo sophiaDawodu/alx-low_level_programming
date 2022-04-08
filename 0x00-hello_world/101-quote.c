6e46967e42ad922e65a76596d1713f11fc809f95
sophiaDawodu <sophiadawodu@gmail.com>
2022-04-07 20:12:57 -0700

    hello world

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+fprintf(stderr, "\n");
+return (1);
+}
---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main - primary function that must be at the beginning of the program
+ *Return:0 - on reaching the end the program should return 0
+*/
+int main(void)
+{
+char msg[] = "\"Programming is like building a multilingual puzzle";
+puts(msg);
+return (0);
+}
