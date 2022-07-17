d844dedbaf2b2747f74f30935390e2974d3838ec
sophiaDawodu <sophiadawodu@gmail.com>
2022-07-16 18:22:30 -0700

    done

---
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget -O /tmp/101-make_me_win.sh https://github.com/sophiaDawodu/alx-low_level_programming/blob/main/0x18-dynamic_libraries/101-make_me_win.sh
+export LD_PRELOAD=/tmp/101-make_me_win.sh
